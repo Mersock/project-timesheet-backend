73037d58c0805fde42353f39b13063dc983d7e70
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-02-19 17:31:48 +0700

    add script init table roles

---
+++ pkg/mysql/init-roles.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `roles`(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) UNIQUE NOT NULL,
+    created_at timestamp NOT NULL,
+    updateed_at timestamp,
+    CONSTRAINT PK_Roles PRIMARY KEY (id)
+);
+
+INSERT INTO `roles` (name,created_at) values ('project manager',CURRENT_TIMESTAMP());
+INSERT INTO `roles` (name,created_at) values ('project member',CURRENT_TIMESTAMP());
+INSERT INTO `roles` (name,created_at) values ('administrator',CURRENT_TIMESTAMP());

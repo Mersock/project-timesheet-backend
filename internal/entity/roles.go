b2a201c8ca7ad6f292f84926ebf661f8db6677e9
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-02-19 21:33:05 +0700

    add pointer roles entity

--- internal/entity/roles.go
+++ internal/entity/roles.go
@@ -2,8 +2,8 @@ package entity
 
 // Roles -.
 type Roles struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	CreateAt string `json:"created_at"`
-	UpdateAt string `json:"updated_at,omitempty"`
+	ID       *int    `json:"id"`
+	Name     *string `json:"name"`
+	CreateAt *string `json:"created_at"`
+	UpdateAt *string `json:"updated_at,omitempty"`
 }

cc00686e374de2d153de0b99751a78f1b3ec7dc7
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-02-20 11:58:02 +0700

    add roles request

---
+++ internal/request/roles.go
@@ -0,0 +1,9 @@
+package request
+
+import "github.com/Mersock/project-timesheet-backend/internal/utils"
+
+// RolesReq -.
+type RolesReq struct {
+	Name string `form:"name" binding:"omitempty"`
+	utils.Pagination
+}

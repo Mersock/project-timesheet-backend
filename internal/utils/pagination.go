fab85721e05900166d1b3b8019601544e98c2b86
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-02-20 12:03:41 +0700

    update utils

--- internal/utils/pagination.go
+++ internal/utils/pagination.go
@@ -4,6 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Pagination -.
+type Pagination struct {
+	Limit    string `form:"limit" json:"limit" binding:"numeric,omitempty"`
+	Page     string `form:"page" json:"page" binding:"numeric,omitempty"`
+	SortBy   string `form:"sortBy" json:"-" binding:"omitempty"`
+	SortType string `form:"sortType" json:"-" binding:"omitempty"`
+	Total    string `json:"total"`
+}
+
 // GeneratePaginationFromRequest -.
 func GeneratePaginationFromRequest(c *gin.Context) Pagination {
 	limit := "10"

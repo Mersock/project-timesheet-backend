d9d8dbb70572b7d520986d1e6903c53449688bdd
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-02-20 08:37:42 +0700

    add pagination response

--- internal/controller/http/v1/roles.go
+++ internal/controller/http/v1/roles.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"github.com/Mersock/project-timesheet-backend/internal/entity"
 	"github.com/Mersock/project-timesheet-backend/internal/usecase"
+	"github.com/Mersock/project-timesheet-backend/internal/utils"
 	"github.com/Mersock/project-timesheet-backend/pkg/logger"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -29,10 +30,15 @@ func newRolesRoutes(handler *gin.RouterGroup, ru usecase.Roles, l logger.Interfa
 type rolesResponse struct {
 	Roles []entity.Roles `json:"data"`
 	Total int            `json:"total"`
+	utils.Pagination
 }
 
 // getRoles -.
 func (r rolesRoutes) getRoles(c *gin.Context) {
+	//pagination
+	paginate := utils.GeneratePaginationFromRequest(c)
+
+	//total rows
 	total, err := r.ru.GetRowsRoles()
 	if err != nil {
 		r.l.Error(err, "http - v1 - Roles")
@@ -46,8 +52,13 @@ func (r rolesRoutes) getRoles(c *gin.Context) {
 		errorResponse(c, http.StatusInternalServerError, "database error")
 		return
 	}
+
 	c.JSON(http.StatusOK, rolesResponse{
 		Roles: roles,
 		Total: total,
+		Pagination: utils.Pagination{
+			Limit: paginate.Limit,
+			Page:  paginate.Page,
+		},
 	})
 }

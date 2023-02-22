9d8480ef47b0027d20dfc2dbdddc83c2b5b3e419
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-02-22 09:45:44 +0700

    update create project request

--- internal/request/project.go
+++ internal/request/project.go
@@ -4,7 +4,7 @@ import "github.com/google/uuid"
 
 // CreateProjectReq -.
 type CreateProjectReq struct {
-	Name   string    `form:"name" json:"name" binding:"required,max=255"`
-	Code   uuid.UUID `form:"-" json:"-" binding:"required,uuid"`
-	UserID int64     `form:"-" json:"-" binding:"required,numeric"`
+	Name        string    `form:"name" json:"name" binding:"required,max=255"`
+	Code        uuid.UUID `form:"-" json:"-" binding:"required,uuid"`
+	UserOwnerID int64     `form:"-" json:"-" binding:"required,numeric"`
 }

d14c886be0c890a0110a9aed92d2668fdc73a24f
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-02-22 20:56:17 +0700

    fix bug signup user

--- internal/request/auth.go
+++ internal/request/auth.go
@@ -2,7 +2,11 @@ package request
 
 // SignUpReq -.
 type SignUpReq struct {
-	CreateUserReq
+	Email     string `form:"email" json:"email" binding:"required,email,max=255"`
+	Password  string `form:"password" json:"password" binding:"required,min=6"`
+	Firstname string `form:"firstname" json:"firstname" binding:"required,max=255"`
+	Lastname  string `form:"lastname" json:"lastname" binding:"required,max=255"`
+	Role      int    `form:"-" json:"_" binding:"numeric"`
 }
 
 // SignInReq -.

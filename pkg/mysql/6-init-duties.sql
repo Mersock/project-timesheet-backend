00d9ccbda2bc7c19a50492bfcef9383209cf8485
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-02-22 09:29:02 +0700

    update init duties command

--- pkg/mysql/6-init-duties.sql
+++ pkg/mysql/6-init-duties.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS `duties`;
 CREATE TABLE IF NOT EXISTS `duties`(
     project_id int NOT NULL,
     user_id int NOT NULL,
+    is_owner bool NOT NULL,
     PRIMARY KEY (project_id, user_id),
     FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE

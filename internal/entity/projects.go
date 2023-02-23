1126da35ce5e4db9b0027e15150feb342cb3fae5
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-02-23 07:34:13 +0700

    add project with user entity

--- internal/entity/projects.go
+++ internal/entity/projects.go
@@ -10,3 +10,17 @@ type Projects struct {
 	CreateAt *time.Time `json:"created_at"`
 	UpdateAt *time.Time `json:"updated_at,omitempty"`
 }
+
+// ProjectsWithUser -.
+type ProjectsWithUser struct {
+	ID        *int       `json:"id"`
+	Name      *string    `json:"name"`
+	Code      *string    `json:"code"`
+	CreateAt  *time.Time `json:"created_at"`
+	UpdateAt  *time.Time `json:"updated_at,omitempty"`
+	UserID    *int       `json:"id"`
+	Email     *string    `json:"email"`
+	Firstname *string    `json:"firstname"`
+	Lastname  *string    `json:"lastname"`
+	Role      *string    `json:"role"`
+}

1265d5cfb359be73b95f97a66018aa57c11528af
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-02-24 00:16:08 +0700

    add custom validate

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,11 +6,14 @@ import (
 	v1 "github.com/Mersock/project-timesheet-backend/internal/controller/http/v1"
 	"github.com/Mersock/project-timesheet-backend/internal/usecase"
 	"github.com/Mersock/project-timesheet-backend/internal/usecase/repo"
+	"github.com/Mersock/project-timesheet-backend/internal/utils"
 	"github.com/Mersock/project-timesheet-backend/pkg/httpserver"
 	"github.com/Mersock/project-timesheet-backend/pkg/logger"
 	"github.com/Mersock/project-timesheet-backend/pkg/mysql"
 	"github.com/Mersock/project-timesheet-backend/pkg/token"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	"os"
 )
 
@@ -23,6 +26,10 @@ func Run(cfg *config.Config) {
 		l.Fatal(fmt.Errorf("app - RUN - mysql.NewMysqlConn: %w", err))
 	}
 	defer db.Close()
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("iso8601date", utils.IsISO8601Date)
+	}
 
 	tokenMaker, err := token.NewJWTMaker(cfg.KEY.TokenSymmetric)
 	if err != nil {
--- internal/utils/validator.go
+++ internal/utils/validator.go
@@ -1,6 +1,9 @@
 package utils
 
-import "github.com/go-playground/validator/v10"
+import (
+	"github.com/go-playground/validator/v10"
+	"regexp"
+)
 
 func GetValidateTag(fe validator.FieldError) string {
 	switch fe.Tag() {
@@ -24,4 +27,11 @@ func GetValidateTag(fe validator.FieldError) string {
 		return "This field allow only alpha and numeric"
 	}
 	return "Unknown error"
+}
+
+func IsISO8601Date(fl validator.FieldLevel) bool {
+	ISO8601DateRegexString := "^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])(?:T|\\s)(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])?(Z)?$"
+	ISO8601DateRegex := regexp.MustCompile(ISO8601DateRegexString)
+
+	return ISO8601DateRegex.MatchString(fl.Field().String())
 }

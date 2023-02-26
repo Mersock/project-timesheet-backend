9dbace610c8e2281907c93e85def38f4f199969f
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-02-26 19:37:01 +0700

    update statuses sql

--- pkg/mysql/7-init-statuses.sql
+++ pkg/mysql/7-init-statuses.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `statuses`(
     name varchar(255) UNIQUE NOT NULL,
     created_at timestamp NOT NULL,
     updated_at timestamp,
-    CONSTRAINT PK_Roles PRIMARY KEY (id)
+    PRIMARY KEY (id)
 );
 
 INSERT INTO `statuses` (name,created_at) values ('to do',CURRENT_TIMESTAMP());

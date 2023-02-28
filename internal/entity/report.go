5603fb4e9e52e3bb9643cdcb93c78b50bd324e88
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-02-28 11:33:07 +0700

    update select column report

--- internal/entity/report.go
+++ internal/entity/report.go
@@ -2,9 +2,10 @@ package entity
 
 // ReportWorkType -.
 type ReportWorkType struct {
+	ProjectID    *int    `json:"project_id"`
+	ProjectName  *string `json:"project_name"`
 	WorkTypeID   *int    `json:"work_type_id"`
 	WorkTypeName *string `json:"work_type_name"`
-	ProjectName  *string `json:"project_name"`
 	TotalSeconds *int    `json:"total_seconds"`
 	TotalTime    *string `json:"total_time"`
 }
--- internal/usecase/repo/report.go
+++ internal/usecase/repo/report.go
@@ -37,23 +37,24 @@ func (r *ReportRepo) SelectWorkType(req request.GetWorkTypeReportReq) ([]entity.
 	var entities []entity.ReportWorkType
 
 	sqlRaw := "SELECT  "
-	sqlRaw += "work_types.id work_type_id, "
-	sqlRaw += "work_types.name work_type_name,"
-	sqlRaw += "projects.name project_name, "
-	sqlRaw += "SUM(TIMESTAMPDIFF(SECOND, start_time , end_time)) as total_seconds "
+	sqlRaw += "projects.id AS project_id, "
+	sqlRaw += "projects.name AS project_name, "
+	sqlRaw += "work_types.id AS work_type_id, "
+	sqlRaw += "work_types.name AS work_type_name,"
+	sqlRaw += "SUM(TIMESTAMPDIFF(SECOND, start_time , end_time)) AS total_seconds "
 	sqlRaw += "FROM time_entries "
 	sqlRaw += "INNER JOIN work_types ON time_entries.work_type_id = work_types.id "
 	sqlRaw += "INNER JOIN projects ON work_types.project_id = projects.id "
 	sqlRaw += "WHERE projects.code = ? "
 	sqlRaw += "GROUP BY work_types.id, work_types.name, projects.name "
-	sqlRaw += "ORDER BY work_types.id"
+	sqlRaw += "ORDER BY projects.id ASC, work_types.id ASC"
 	results, err := r.DB.Query(sqlRaw, req.ProjectCode)
 	if err != nil {
-		return entities, fmt.Errorf("ReportRepo - Count - r.DB.QueryRow: %w", err)
+		return entities, fmt.Errorf("ReportRepo - Count - r.DB.Query: %w", err)
 	}
 	for results.Next() {
 		var e entity.ReportWorkType
-		err = results.Scan(&e.WorkTypeID, &e.WorkTypeName, &e.ProjectName, &e.TotalSeconds)
+		err = results.Scan(&e.ProjectID, &e.ProjectName, &e.WorkTypeID, &e.WorkTypeName, &e.TotalSeconds)
 		entities = append(entities, e)
 	}
 

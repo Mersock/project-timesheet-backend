1921bfe1ea48d03c40071666fee25271e3853b2d
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-04-25 23:03:02 +0700

    update middleware

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Mersock/project-timesheet-backend/config"
 	v1 "github.com/Mersock/project-timesheet-backend/internal/controller/http/v1"
+	"github.com/Mersock/project-timesheet-backend/internal/middleware"
 	"github.com/Mersock/project-timesheet-backend/internal/usecase"
 	"github.com/Mersock/project-timesheet-backend/internal/usecase/repo"
 	"github.com/Mersock/project-timesheet-backend/internal/utils"
@@ -13,7 +14,6 @@ import (
 	"github.com/Mersock/project-timesheet-backend/pkg/logger"
 	"github.com/Mersock/project-timesheet-backend/pkg/mysql"
 	"github.com/Mersock/project-timesheet-backend/pkg/token"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -71,11 +71,7 @@ func Run(cfg *config.Config) {
 	handler := gin.New()
 
 	// allow cors
-	handler.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"*"},
-		AllowMethods: []string{"POST", "PUT", "PATCH", "DELETE"},
-		AllowHeaders: []string{"Content-Type,access-control-allow-origin, access-control-allow-headers"},
-	}))
+	handler.Use(middleware.CORSMiddleware())
 
 	v1.NewRouter(
 		handler,
